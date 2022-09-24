87da6c73379aae8c99af82d662aae03426b60120
Mageta Sukulu <magetasukulu@gmail.com>
2022-09-24 12:25:09 +0300

    Update

---
+++ todoapp/src/App.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import Todos from './todos.js'
+
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'buy some milk'},
+      {id: 2, content: 'play mario kart'}
+    ]
+  }
+  
+  render() {    
+    return (
+      <div className="App">
+        <h1 className="center blue-text">
+          Todo's
+        </h1>
+
+        <Todos todos={this.state.todos} />
+        
+      </div>
+    );
+  }
+}
+
+export default App;
