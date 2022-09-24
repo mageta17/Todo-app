8d34702d5b3a16a7724510e1e3644728e0450f97
Mageta Sukulu <magetasukulu@gmail.com>
2022-09-24 12:38:50 +0300

    Delete function

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -8,15 +8,23 @@ class App extends Component {
       {id: 2, content: 'play mario kart'}
     ]
   }
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id 
+    });
+    this.setState({
+      todos: todos
+    })
+  }
   
   render() {    
     return (
-      <div className="App">
+      <div className="todo-app container">
         <h1 className="center blue-text">
           Todo's
         </h1>
 
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
         
       </div>
     );
